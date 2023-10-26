2da2255456e3cefe4a62f7130d6417f5b9ad66a0
chanyu <trafalgarlaw0816@gmail.com>
2023-10-26 12:28:12 +0800

    Convert image to base64 code for pyinstaller

---
+++ GUI/App/ImageConvert.py
@@ -0,0 +1,16 @@
+import base64
+
+
+def pic2str(file, functionName):
+    pic = open(file, 'rb')
+    content = '{} = {}\n'.format(functionName, base64.b64encode(pic.read()))
+    pic.close()
+
+    with open('ImageConvertResult.py', 'a') as f:
+        f.write(content)
+
+
+if __name__ == '__main__':
+    pic2str('./images/LoginBg.png', 'LoginBg')
+    pic2str('./images/LoginPageBg.png', 'LoginPageBg')
+    pic2str('./images/ObjectDetectionPageBg.png', 'ObjectDetectionBg')
