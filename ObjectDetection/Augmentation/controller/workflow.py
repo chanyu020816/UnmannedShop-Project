59ea27e46f27a45f37fd4e7b643f0589b832ae25
chanyu <trafalgarlaw0816@gmail.com>
2023-10-29 01:59:54 +0800

    Modify Augmentation Code

--- ObjectDetection/Augmentation/controller/workflow.py
+++ ObjectDetection/Augmentation/controller/workflow.py
@@ -3,7 +3,7 @@ from controller.get_album_bb import get_bboxes_list
 import cv2
 import os
 import yaml
-
+import time
 
 with open("contants.yaml", 'r') as stream:
     CONSTANTS = yaml.safe_load(stream)
@@ -18,6 +18,7 @@ def run_pipeline():
         image = cv2.imread(os.path.join(CONSTANTS["inp_img_pth"], img_file))           
         lab_pth = os.path.join(CONSTANTS["inp_lab_pth"], file_name + '.txt')                                
         album_bboxes = get_bboxes_list(lab_pth, CONSTANTS['CLASSES'])
-        for x in range(10):
+        for x in range(30):
             aug_file_name = file_name + "_" + CONSTANTS["transformed_file_name"] + str(x)
             apply_aug(image, album_bboxes, CONSTANTS["out_lab_pth"],  CONSTANTS["out_img_pth"], aug_file_name, CONSTANTS['CLASSES'])
+        time.sleep(5)
