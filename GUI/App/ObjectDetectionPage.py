68cb78ea89103e9c9aa9666173bba679e130335e
chanyu <trafalgarlaw0816@gmail.com>
2023-11-02 00:26:46 +0800

    Complete Upload and Detect function, also create a TreeView to display the detected result

--- GUI/App/ObjectDetectionPage.py
+++ GUI/App/ObjectDetectionPage.py
@@ -7,22 +7,20 @@ from ImageConvertResult import ObjectDetectionBg
 import base64
 from io import BytesIO
 from sys import platform
-
 if platform == "darwin":
     from tkmacosx import Button
 else:
     from tkinter import Button
 import cv2
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
 cap = cv2.VideoCapture(0)
-#cap.set(3, 0)
-#cap.set(4, 550)
-import random
+
 from numpy import array
 from ultralytics import YOLO
 model = YOLO("./best.pt")
 class_list = ['Bak Kut Teh Flavor Noodles', 'Doritos', 'I MEI-Milk Puff', 'M-M-Crisp', 'M-M-Peanut', 'Oreo', 'Popconcern-Sweet-Salty', 'Pringles-Origin', 'PureTea-Black Tea', 'PureTea-LemonGreen Tea', 'Skittles', 'White Chocolate Ice Cream']
 color_list = ["#FF5733", "#42A5F5", "#7B8D8C", "#E57373", "#FFD700", "#4CAF50", "#9C27B0", "#FF5722", "#607D8B", "#FFD600", "#795548", "#E91E63"]
-
 
 class ObjectDetectionPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -47,6 +45,7 @@ class ObjectDetectionPage(tk.Frame):
         self.captured_image = None
         self.capture_result = None
         self.nothing_found = None
+        self.retake_button = None
 
         # Set up the frame's dimensions
         self.configure(width=1400, height=800)
@@ -56,63 +55,72 @@ class ObjectDetectionPage(tk.Frame):
         self.create_object_detection_page()
 
     def create_object_detection_page(self):
-        # data
-        #first_names = ['Bob', 'Maria', 'Alex', 'James', 'Susan', 'Henry', 'Lisa', 'Anna', 'Lisa']
-        #last_names = ['Smith', 'Brown', 'Wilson', 'Thomson', 'Cook', 'Taylor', 'Walker', 'Clark']
 
-        # treeview
-        #table = ttk.Treeview(self, columns=('first', 'last', 'email'), show='headings')
-        #table.heading('first', text='First name')
-        #table.heading('last', text='Surname')
-        #table.heading('email', text='Email')
-        #table.place(x = 200, y = 200)
+        style = ttk.Style()
+        # style.theme_use('clam')
+        style.configure("Treeview", fieldbackground="#FFFFFF", foreground="#000000",
+            background="#FFFFFF", rowheight=28)
+        style.configure("Treeview.Heading", font=("Open Sans", 20, 'bold'), background="#ECD7AA", foreground="#4f4e4d")
 
-        # insert values into a table
-        # table.insert(parent = '', index = 0, values = ('John', 'Doe', 'JohnDoe@email.com'))
-        #for i in range(100):
-        #    first = choice(first_names)
-        #    last = choice(last_names)
-        #    email = f'{first[0]}{last}@email.com'
-        #    data = (first, last, email)
-        #    table.insert(parent='', index=0, values=data)
+        # Object Detection Result
+        self.table = ttk.Treeview(self, columns=('item_id', 'item_name', 'item_num', 'total_price'), show='headings',
+            style="Treeview")
+        self.table.heading("#0", text="qwer")
+        self.table.column("#0", width=100)
+        self.table.heading('item_id', text='Item ID')
+        self.table.column("item_id", width=100)
+        self.table.heading('item_name', text='Item Name')
+        self.table.column("item_name", width=250)
+        self.table.heading('item_num', text='Number')
+        self.table.column("item_num", width=100)
+        self.table.heading('total_price', text='Total Price')
+        self.table.column("total_price", width=150)
+        self.table.place(x=730, y=300)
+
         ## FaceRecog TITLE ##
-        ObjectDetect_Title = tk.Label(self, text="Object   Detection", font=("Canva Sans", 30, "bold"),
-                                   bg="#FFF3F3", fg="#4f4e4d")
-        ObjectDetect_Title.place(x=280, y=80)
+        ObjectDetect_Title = tk.Label(self, text="Object   Detection", font=("Canva Sans", 35, "bold"),
+                                   bg="#FFFFFF", fg="#4f4e4d")
+        ObjectDetect_Title.place(x=220, y=65)
         self.canvas = tk.Canvas(self, bg="#FFFFFF", bd=0, borderwidth=0, border=0, relief="solid", width=350,
                            highlightthickness=0)
         #self.canvas.place(x=50, y=150)
 
-        TakeImage_button = Button(self, text="Detect   Items", command=self.capture,
+        self.TakeImage_button = Button(self, text="Detect   Items", command=self.capture,
              padx=10, pady=8, bg="#DF3F3F", bd=0, font=("Open Sans", 20, "bold"),
              activebackground="#FF3A3A", activeforeground="white", fg="white", highlightthickness=0,
-             borderwidth=0,
-             highlightcolor="#FFF3F3", highlightbackground="#FFF3F3", width=250, height=40)
-        TakeImage_button.place(x=1080, y=380)
+             borderwidth=0, highlightcolor="#FFF3F3", highlightbackground="#FFF3F3", width=280, height=40)
+        self.TakeImage_button.place(x=400, y=680)
 
-
-        self.showInfo_button = Button(self, text="Show User Info", command=self.Display, padx = 10, pady=8,
+        self.showInfo_button = Button(self, text="Show User Info", command=self.Display, padx=10, pady=8,
             bg="#DF3F3F", bd = 0, font=("Open Sans", 20, "bold"), activebackground="#FF3A3A",
             activeforeground = "#FF3A3A", fg="white", highlightthickness=0, borderwidth=0, highlightcolor="#FFF3F3",
             highlightbackground="#FFF3F3", width=250)
-        self.showInfo_button.place(x=970, y=80)
+        self.showInfo_button.place(x=900, y=80)
 
+        logout_button = Button(self, text="Logout", command=self.controller.show_login_page, padx=10, pady=8,
+            bg="#DF3F3F", bd=0, font=("Open Sans", 15, "bold"), activebackground="#FF3A3A",
+            activeforeground="#FF3A3A", fg="white", highlightthickness=0, borderwidth=0, highlightcolor="#FFF3F3",
+            highlightbackground="#FFF3F3", width=100, height=30)
+        logout_button.place(x=1265, y=40)
 
+        self.retake_button = Button(self, text="ReDetect   Items", command=self.retake,
+             padx=10, pady=8, bg="#DF3F3F", bd=0, font=("Open Sans", 20, "bold"),
+             activebackground="#FF3A3A", activeforeground="white", fg="white", highlightthickness=0,
+             borderwidth=0,
+             highlightcolor="#FFF3F3", highlightbackground="#FFF3F3", width=280, height=40)
 
+        upload_button = Button(self, text="Upload   Image", command=self.upload_image,
+             padx=10, pady=8, bg="#DF3F3F", bd=0, font=("Open Sans", 20, "bold"),
+             activebackground="#FF3A3A", activeforeground="white", fg="white", highlightthickness=0,
+             borderwidth=0,
+             highlightcolor="#FFF3F3", highlightbackground="#FFF3F3", width=280, height=40)
+        upload_button.place(x=80, y=680)
 
-        # table.insert(parent='', index=tk.END, values=('XXXXX', 'YYYYY', 'ZZZZZ'))
-
-        back_to_login_button = tk.Button(self, text="Back to Login", command=self.controller.show_login_page)
-        back_to_login_button.place(x=100, y=80)
-
-        retake_button = tk.Button(self, text="Back to Login", command=self.retake)
-        retake_button.place(x=200, y=80)
-
-        purchase_button = Button(self, text="Confirm Purchase", command=self.controller.show_finish_purchase_page,
-            padx=10, pady=8, bg="#DF3F3F", bd=0, font=("Open Sans", 20, "bold"),
+        self.purchase_button = Button(self, text="Confirm   Purchase", command=self.controller.show_finish_purchase_page,
+            padx=10, pady=8, bg="#DF3F3F", bd=0, font=("Open Sans", 25, "bold"),
             activebackground="#FF3A3A", activeforeground="white", fg="white", highlightthickness=0, borderwidth=0,
-            highlightcolor="#FFF3F3", highlightbackground="#FFF3F3", width=250, height=40)
-        purchase_button.place(x=1080, y=680)
+            highlightcolor="#FFF3F3", highlightbackground="#FFF3F3", width=280, height=50)
+
 
         #self.show_frame(canvas)
 
@@ -122,51 +130,50 @@ class ObjectDetectionPage(tk.Frame):
         if self.showInfo_button:
             self.showInfo_button.place_forget()
         self.ShowUserInfoLabel = tk.Label(self, text=f"User Information",
-                                         font=("yu gothic ui", 35, "bold"),
-                                         bg="#FFF3F3", fg="#4f4e4d")
-        self.ShowUserInfoLabel.place(x=900, y=80)
-        self.FaceDetectUserID = tk.Label(self, text=f"User ID: {login_user_ID}",
                                          font=("yu gothic ui", 25, "bold"),
                                          bg="#FFF3F3", fg="#4f4e4d")
-        self.FaceDetectUserID.place(x=800, y=140)
+        self.ShowUserInfoLabel.place(x=930, y=80)
+        self.FaceDetectUserID = tk.Label(self, text=f"User ID: {login_user_ID}",
+                                         font=("yu gothic ui", 20, "bold"),
+                                         bg="#FFF3F3", fg="#4f4e4d")
+        self.FaceDetectUserID.place(x=800, y=120)
 
         self.FDusername = "UnDetected"
         self.FaceDetectUserName = tk.Label(self, text=f"User Name:  {login_user_name}",
-                                           font=("yu gothic ui", 25, "bold"),
+                                           font=("yu gothic ui", 20, "bold"),
                                            bg="#FFF3F3", fg="#4f4e4d")
-        self.FaceDetectUserName.place(x=800, y=180)
+        self.FaceDetectUserName.place(x=800, y=150)
 
     def show_frame(self, canvas):
         if not self.running_object_detect_frame:
             return
-
         self.image_id = 0  # inform function to assign new value to global variable instead of local variable
         if self.captured_image is not None:
-            print(self.model_result)
+
             if len(self.model_result) == 0:
                 self.nothing_found = tk.Label(self, text="No Item Detected", font=("Canva Sans", 35, "bold"),
-                    bg="#FFF3F3", fg="#4f4e4d")
-                self.nothing_found.place(x=1000, y=600)
+                    bg="#FFF3F3", fg="#DF3F3F")
+                self.nothing_found.place(x=880, y=250)
             else:
-                if self.nothing_found:
+
+                if self.nothing_found is not None:
                     self.nothing_found.place_forget()
-                initial_height = 300
+
                 for item in self.model_result:
-                    self.capture_result = tk.Label(self, text=item, font=("Canva Sans", 35, "bold"),
-                    bg="#FFF3F3", fg="#4f4e4d")
-                    self.capture_result.place(x=1000, y=initial_height)
-                    initial_height += 50
-            # photo = ImageTk.PhotoImage(image=self.captured_image)
-            # solution for bug in `PhotoImage`
-            #self.canvas.photo = self.captured_image
+                    data = (1, item, 2, 20)
+                    self.table.insert(parent='', index=0, values=data)
+                self.purchase_button.place(x=880, y=650)
+
         else:
             if self.capture_result:
                 self.capture_result.place_forget()
+            if self.nothing_found:
+                self.nothing_found.place_forget()
             # get frame
             ret, frame = cap.read()
             # Set the desired capture width and height
-            video_width = 640  # Change this to your preferred width
-            video_height = 640  # Change this to your preferred height
+            video_width = 600
+            video_height = 540
 
             # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 350)  # 3 corresponds to CV_CAP_PROP_FRAME_WIDTH
             # cap.set(4, video_height)  # 4 corresponds to CV_CAP_PROP_FRAME_HEIGHT
@@ -187,7 +194,7 @@ class ObjectDetectionPage(tk.Frame):
 
                 # coordinates
                 for r in results:
-                    print(random.randint(1, 1))
+                    # print(random.randint(1, 1))
                     boxes = r.boxes
                     self.model_result = []
                     for box in boxes:
@@ -242,14 +249,15 @@ class ObjectDetectionPage(tk.Frame):
             # Convert the captured frame to a PhotoImage
             img = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2RGB)
 
-            cv2.imwrite("./test.jpg", flipped_frame)
-
             self.captured_image = ImageTk.PhotoImage(image=Image.fromarray(img))
 
         self.capturing = False
+        self.TakeImage_button.place_forget()
+        self.retake_button.place(x=400, y=680)
 
     def run(self):
         self.window.mainloop()
+
     def stop_show_frame(self):
         self.running_object_detect_frame = False
         self.canvas.place_forget()
@@ -257,8 +265,76 @@ class ObjectDetectionPage(tk.Frame):
     def resume_show_frame(self):
         self.running_object_detect_frame = True
         self.show_frame(self.canvas)
-        self.canvas.place(x=120, y=150)
+        self.canvas.place(x=80, y=130)
 
     def retake(self):
+        for item in self.table.get_children():
+            self.table.delete(item)
         self.captured_image = None
+        if self.nothing_found is not None:
+            self.nothing_found.place_forget()
+        if self.retake_button:
+            self.retake_button.place_forget()
+        self.TakeImage_button.place(x=400, y=680)
         self.resume_show_frame()
+        self.purchase_button.place_forget()
+
+    def upload_image(self):
+        if self.nothing_found is not None:
+            self.nothing_found.place_forget()
+        self.retake()
+        self.after(500)
+        file_path = askopenfilename(filetypes=[('Jpg Files', '*.jpg'), ('PNG Files','*.png')])
+        if file_path:
+            #
+            # Open and display the selected image
+            image = Image.open(file_path)
+            image = image.resize((600, 540))
+            #photo = ImageTk.PhotoImage(image)
+
+            self.captured_image = ""
+            self.TakeImage_button.place_forget()
+            self.retake_button.place(x=400, y=680)
+            ## Model Predict ##
+            results = model(image, verbose=False, stream=True)
+            video_width = 600  # Change this to your preferred width
+            video_height = 540
+            # coordinates
+            for r in results:
+                # print(random.randint(1, 1))
+                boxes = r.boxes
+                self.model_result = []
+                for box in boxes:
+                    # bounding box
+                    x1, y1, x2, y2 = box.xyxy[0]
+
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # convert to int values
+
+                    # class name
+                    cls = int(box.cls[0])
+                    self.model_result.append(class_list[cls])
+
+                    # put box in cam
+                    # Convert a hex color to BGR
+                    hex_color = color_list[cls]
+                    bgr_color = tuple(int(hex_color[i:i + 2], 16) for i in (1, 3, 5))
+                    img_t = cv2.rectangle(array(image), (x1, y1), (x2, y2), bgr_color, thickness=2)
+
+                    # print("Class name ", class_list[cls])
+                    org = [x1, y1]
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    fontScale = 1
+                    thickness = 2
+                    # self.captured_image = ""
+                    img_t = cv2.putText(img_t, class_list[cls], org, font, fontScale, bgr_color, thickness)
+                    img = Image.fromarray(img_t)
+                    image = img.resize((video_width, video_height), Image.ANTIALIAS)
+            #if self.nothing_found is not None:
+            #    self.nothing_found.place_forget()
+            photo = ImageTk.PhotoImage(image=image)
+            self.canvas.create_image(0, 0, anchor="nw", image=photo)
+            self.canvas.image = photo
+            self.canvas.place(x=80, y=110)
+
+    def DisplayItems(self):
+        ...
