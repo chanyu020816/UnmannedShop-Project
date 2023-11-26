6a081254f28dda85c13ff86614b6953928dfee0f
chanyu <trafalgarlaw0816@gmail.com>
2023-11-26 19:48:22 +0800

    Add Display function

--- GUI/App/main.py
+++ GUI/App/main.py
@@ -32,22 +32,17 @@ class MainView(tk.Tk):
     def show_create_account_page(self):
         self.pages["CreateAccountPage"].grid(row=0, column=0, sticky="nsew")
         self.pages["LoginPage"].grid_forget()
-        self.pages["ObjectDetectionPage"].grid_forget()
-        self.pages["DonePurchasePage"].grid_forget()
-        self.pages["FinishPurchasePage"].grid_forget()
         self.pages["LoginPage"].stop_show_frame()
         self.after(50, self.pages["CreateAccountPage"].resume_show_frame())
 
     def show_object_detection_page(self):
         self.pages["ObjectDetectionPage"].grid(row=0, column=0, sticky="nsew")
         self.pages["LoginPage"].grid_forget()
-        self.pages["CreateAccountPage"].grid_forget()
         self.pages["FinishPurchasePage"].grid_forget()
-        self.pages["DonePurchasePage"].grid_forget()
         self.pages["LoginPage"].stop_show_frame()
+        self.pages["ObjectDetectionPage"].Display()
         #self.pages["ObjectDetectionPage"].resume_show_frame()
         self.after(50, self.pages["ObjectDetectionPage"].resume_show_frame())
-
 
     def show_finish_purchase_page(self):
         self.pages["FinishPurchasePage"].grid(row=0, column=0, sticky="nsew")
@@ -76,7 +71,6 @@ class MainView(tk.Tk):
     def AfterSignUpAccount_show_login_page(self):
         self.pages["LoginPage"].grid(row=0, column=0, sticky="nsew")
         self.pages["CreateAccountPage"].grid_forget()
-        self.pages["ObjectDetectionPage"].grid_forget()
         self.pages["CreateAccountPage"].stop_show_frame()
         self.pages["LoginPage"].reloadModel()
         self.pages["LoginPage"].resume_show_frame()
